bd2f19270a62c680a61cd501692907d6298ba18c
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2018-11-21 13:14:37 +0200

    Bump version from 0.2.1 to 0.2.2
    Minor improvements for Django 2 support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-dynamic-subdomains-ai',
     description="Dynamic and static subdomain support for Django.",
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/andersinno/django-dynamic-subdomains',
 
     author='Anders',
